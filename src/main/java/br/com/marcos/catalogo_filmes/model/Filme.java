847999c0ebef99e1923933f297d0ff4423d35188
Marcos Willian <marcoswms@gmail.com>
2026-06-22 01:18:03 -0300

    feat: adiciona dependencias do projeto. adiciona a entidade 'Filmes'

---
+++ src/main/java/br/com/marcos/catalogo_filmes/model/Filme.java
@@ -0,0 +1,68 @@
+package br.com.marcos.catalogo_filmes.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "filmes")
+public class Filme {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String titulo;
+    private String genero;
+    private Integer anoLancamento;
+    private Integer nota;
+    private Boolean assistido;
+
+    public Filme() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public Integer getAnoLancamento() {
+        return anoLancamento;
+    }
+
+    public void setAnoLancamento(Integer anoLancamento) {
+        this.anoLancamento = anoLancamento;
+    }
+
+    public Integer getNota() {
+        return nota;
+    }
+
+    public void setNota(Integer nota) {
+        this.nota = nota;
+    }
+
+    public Boolean getAssistido() {
+        return assistido;
+    }
+
+    public void setAssistido(Boolean assistido) {
+        this.assistido = assistido;
+    }
+}
---
+++ src/test/java/br/com/marcos/catalogo_filmes/CatalogoFilmesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcos.catalogo_filmes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoFilmesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
