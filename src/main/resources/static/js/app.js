08118426fbf1132a5ec78264cc42b54f95e62744
Marcos Willian <marcoswms@gmail.com>
2026-07-12 03:45:24 -0300

    feat: adiciona estrutura em javascript para interação com html (DOM)

---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    const formulario = document.getElementById("formFilme");
+
+    formulario.addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        const campoTitulo = document.getElementById("titulo");
+        const campoGenero = document.getElementById("genero");
+        const campoAnoLancamento = document.getElementById("ano");
+        const campoNota = document.getElementById("nota");
+        const campoAssistido = document.getElementById("assistido");
+
+        const titulo = campoTitulo.value;
+        const genero = campoGenero.value;
+        const anoLancamento = Number(campoAnoLancamento.value);
+        const nota = Number(campoNota.value);
+        const assistido = campoAssistido.checked;
+
+        const filme = {
+                    titulo: titulo,
+                    genero: genero,
+                    anoLancamento: anoLancamento,
+                    nota: nota,
+                    assistido: assistido
+        };
+
+        console.log(filme);
+        console.log("Formulário enviado!");
+    });
+});
