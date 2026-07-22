527a618c37fa57b2e07fcad95ad9687f674f8c3c
Marcos Willian <marcoswms@gmail.com>
2026-07-22 00:29:08 -0300

    feat: adicionar edicao de recurso e separacao de responsabilidades para atualizar e salvar recurso

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,21 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
-    let listaFilmes = [];
 
+    const formulario = document.getElementById("formFilme");
     const tabela = document.getElementById("listaFilmes");
-    tabela.innerHTML = "";
+
+    const campoTitulo = document.getElementById("titulo");
+    const campoGenero = document.getElementById("genero");
+    const campoAnoLancamento = document.getElementById("ano");
+    const campoNota = document.getElementById("nota");
+    const campoAssistido = document.getElementById("assistido");
+
+    let idFilmeEmEdicao = null;
+    let listaFilmes = [];
 
     carregarFilmes();
 
-    const formulario = document.getElementById("formFilme");
-
     formulario.addEventListener("submit", function(event) {
         event.preventDefault();
-
-        const campoTitulo = document.getElementById("titulo");
-        const campoGenero = document.getElementById("genero");
-        const campoAnoLancamento = document.getElementById("ano");
-        const campoNota = document.getElementById("nota");
-        const campoAssistido = document.getElementById("assistido");
 
         const titulo = campoTitulo.value;
         const genero = campoGenero.value;
@@ -31,25 +31,12 @@ document.addEventListener("DOMContentLoaded", function() {
             assistido: assistido
         };
 
-        fetch ("http://localhost:8080/filmes", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(filme)
-        })
-            .then(function(response) {
-
-            if (response.ok) {
-                console.log("Filme salvo com sucesso!");
-                formulario.reset();
-
-                carregarFilmes();
-            }
-            else {
-                console.log(response.status, "Precisamos tratar esse erro!");
-            }
-        });
+        if (idFilmeEmEdicao === null) {
+            salvarFilme(filme);
+        }
+        else {
+            atualizarFilme(filme);
+        }
     });
 
     function carregarFilmes() {
@@ -73,21 +60,38 @@ document.addEventListener("DOMContentLoaded", function() {
                     <td>${filme.nota}</td>
                     <td>${filme.assistido}</td>
                     <td>
-                        <button type="button" class="btn btn-outline-warning btn-sm me-2">Editar</button>
+                        <button type="button" data-id="${filme.id}" class="btn-editar btn btn-outline-warning btn-sm me-2">Editar</button>
                         <button type="button" data-id="${filme.id}" class="btn-excluir btn btn-outline-danger btn-sm me-2">Excluir</button>
                     </td>
                 </tr>`;
             });
+
             tabela.innerHTML = linhas;
+            const botoesEditar = document.querySelectorAll(".btn-editar");
             const botoesExcluir = document.querySelectorAll(".btn-excluir");
 
-            botoesExcluir.forEach(function(botao){
-                botao.addEventListener("click", function(){
+            botoesEditar.forEach(function(botao){
+                botao.addEventListener("click", function() {
+                    const id = botao.dataset.id;
+                    const filme = listaFilmes.find(function(filme) {
+                        return filme.id == id;
+                    });
+                    idFilmeEmEdicao = filme.id;
+
+                    campoTitulo.value = filme.titulo;
+                    campoGenero.value = filme.genero;
+                    campoAnoLancamento.value = filme.anoLancamento;
+                    campoNota.value = filme.nota;
+                    campoAssistido.checked = filme.assistido;
+                });
+            });
+
+            botoesExcluir.forEach(function(botao) {
+                botao.addEventListener("click", function() {
                     const id = botao.dataset.id;
                     excluirFilme(id);
                 });
             });
-
         });
     }
 
@@ -103,6 +107,54 @@ document.addEventListener("DOMContentLoaded", function() {
             }
             else {
                 console.log(`Código ${response.status} - Algo inesperado aconteceu, precisamos tratar!`);
+            }
+        });
+    }
+
+    function atualizarFilme(filme) {
+
+        fetch (`http://localhost:8080/filmes/${idFilmeEmEdicao}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(filme)
+        })
+            .then(function(response) {
+
+            if (response.ok) {
+                console.log("Filme salvo com sucesso!");
+                formulario.reset();
+
+                idFilmeEmEdicao = null;
+
+                carregarFilmes();
+            }
+            else {
+                console.log(response.status, "Precisamos tratar esse erro!");
+            }
+        });
+    }
+
+    function salvarFilme(filme) {
+
+        fetch ("http://localhost:8080/filmes", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(filme)
+        })
+            .then(function(response) {
+
+            if (response.ok) {
+                console.log("Filme atualizado com sucesso!");
+                formulario.reset();
+
+                carregarFilmes();
+            }
+            else {
+                console.log(response.status, "Precisamos tratar esse erro!");
             }
         });
     }
